4dd41b853a018650af3d4f4aab2ded6c54277a93
Luke O'Sullivan <luke@Lukes-MacBook.home>
2019-03-26 17:12:13 -0400

    committing for deployment

--- config/config.js
+++ config/config.js
@@ -17,10 +17,7 @@ module.exports={
       "dialect": "mysql"
     },
     "production": {
-      "username": "root",
-      "password": null,
-      "database": "database_production",
-      "host": "127.0.0.1",
+      "use_env_variable": process.env.JAWSDB_URL,
       "dialect": "mysql"
     }
   }
