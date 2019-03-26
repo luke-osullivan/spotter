b7262f5029487e56740f8dabbd029b5ed1dc4bf1
Luke O'Sullivan <luke@Lukes-MacBook.home>
2019-03-26 14:39:19 -0400

    fixed config for env

--- config/config.js
+++ config/config.js
@@ -1,21 +1,24 @@
-{
+//import dotenv dependencies
+require("dotenv").config();
+
+module.exports={
     "development": {
       "username": "root",
-      "password": "SQLPASS",
+      "password": process.env.SQL_PASS,
       "database": "sequelize_json",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
     "test": {
       "username": "root",
-      "password": "SQLPASS",
+      "password": null,
       "database": "database_test",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
     "production": {
       "username": "root",
-      "password": "SQLPASS",
+      "password": null,
       "database": "database_production",
       "host": "127.0.0.1",
       "dialect": "mysql"
