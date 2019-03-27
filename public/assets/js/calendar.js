b78986ef4bb7b8e7491fed452de5023271b4db00
Luke O'Sullivan <luke@Lukes-MacBook.home>
2019-03-26 23:29:19 -0400

    most recent section fixed, pseudo removed, ready for deploy

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -1,11 +1,5 @@
 YUI().use('calendar', 'datatype-date', 'cssbutton', function (Y) {
 
-
-  // Create a new instance of calendar, placing it in
-  // #mycalendar container, setting its width to 340px,
-  // the flags for showing previous and next month's
-  // dates in available empty cells to true, and setting
-  // the date to today's date.
   var calendar = new Y.Calendar({
     contentBox: "#mycalendar",
     showPrevMonth: true,
@@ -13,17 +7,11 @@ YUI().use('calendar', 'datatype-date', 'cssbutton', function (Y) {
     date: new Date()
   }).render();
 
-  // Get a reference to Y.DataType.Date
   var dt = Y.DataType.Date;
-  console.log(dt);
 
-  // Listen to calendar's selectionChange event.
   calendar.on("selectionChange", function (ev) {
     $("#workout-name").empty();
     $("#exercise-div").empty();
-    
-
-    // Get the date from the list of selected
 
     var newDate = ev.newSelection[0];
 
@@ -42,22 +30,16 @@ YUI().use('calendar', 'datatype-date', 'cssbutton', function (Y) {
     }
 
     var theDate = year + "-" + month + "-" + day;
-    console.log(theDate);
-    // Format the date and output it to a DOM
-    // element.
+
     Y.one("#selecteddate").setHTML(theDate);
 
     $.ajax({
       url: "api/users/status",
       method: "GET"
     }).then(function (userResult) {
-      //console.log(userResult.id);
+
       let activeUser = userResult.id;
       console.log(activeUser);
-     // for (let e = 0; e < userResult.length; e++) {
-        //let eachUser = userResult[e].id
-       // console.log(userResult[e].id);
-     // }
     
 
 
@@ -65,13 +47,9 @@ YUI().use('calendar', 'datatype-date', 'cssbutton', function (Y) {
       url: "api/users/status",
       method: "GET"
     }).then(function (userResult) {
-      //console.log(userResult.id);
       let activeUser = userResult.id;
       console.log(activeUser);
-     // for (let e = 0; e < userResult.length; e++) {
-        //let eachUser = userResult[e].id
-       // console.log(userResult[e].id);
-     // }
+
     
 
 
@@ -79,13 +57,9 @@ YUI().use('calendar', 'datatype-date', 'cssbutton', function (Y) {
       url: "api/workouts",
       method: "GET"
     }).then(function (results) {
-     // console.log(results);
-
 
         for (let i = 0; i < results.length; i++) {
-          //console.log(results[i]);
           let userWorkoutId = results[i].UserId;
-          console.log(userWorkoutId);
           
           if (results[i].date === theDate && activeUser === userWorkoutId) {
 
@@ -98,33 +72,18 @@ YUI().use('calendar', 'datatype-date', 'cssbutton', function (Y) {
           let dbData = results[i];
           
           dbData.exercises.forEach(exercise => {
-
-
-           // specific exercise
            let exerciseName = exercise.specific_exercise;
-           //# console.log("Exercise Name:" + exerciseName);
 
-
-            // set count
-          //  let setsCount = exercise.sets.length;
-            //console.log("Sets:" + setCount);
             $("#exercise-div").append(`
             <br>
             <p>${exerciseName}</p>
             `)
 
-            //console.log(exercise.sets); 
             exercise.sets.forEach(set => {
-              //console.log(set);
 
               let repsCount = set.reps;
-              // console.log("Reps:" + set.reps);
 
               let weightCount = set.weight;
-             // console.log("Weight:" + set.weight);
-            
-
-              //APPEND================================
 
               $("#exercise-div").append(`
               <p>Reps: ${repsCount} reps x ${weightCount} lbs</p>
@@ -138,18 +97,11 @@ YUI().use('calendar', 'datatype-date', 'cssbutton', function (Y) {
   
   });
 
-
-    // When the 'Show Previous Month' link is clicked,
-    // modify the showPrevMonth property to show or hide
-    // previous month's dates
     Y.one("#togglePrevMonth").on('click', function (ev) {
       ev.preventDefault();
       calendar.set('showPrevMonth', !(calendar.get("showPrevMonth")));
     });
 
-    // When the 'Show Next Month' link is clicked,
-    // modify the showNextMonth property to show or hide
-    // next month's dates
     Y.one("#toggleNextMonth").on('click', function (ev) {
       ev.preventDefault();
       calendar.set('showNextMonth', !(calendar.get("showNextMonth")));
--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -13,92 +13,59 @@ $(document).ready(function () {
       <h6>Bio: ${response.bio}</h6>`;
 
       $(".userInfo").append(userInfo);
-
-      // var userPicture =
-
-      // `<img src="${response.picture} height="50" width="50">`
-      // ;
-
-
     })
   };
   printUser();
 
   function printWorkout() {
-    
     $.ajax({
       url: "api/users/status",
       method: "GET"
-    }).done(function (userResult) {
-      //console.log(userResult.id);
+    }).then(function (userResult) {
       let activeUser = userResult.id;
-      console.log(activeUser);
 
       $.ajax({
         url: "api/workouts",
         method: "GET"
-      }).done(function (results) {
-       console.log(results);
+      }).then(function (results) {
+        let workoutDates = [];
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].UserId === activeUser) {
 
-        results.forEach(resDates => {
-
-      if (resDates.UserId === activeUser) {
-        console.log(resDates);
-       let workoutDates = [];
-        let userArray = [];
-        let workoutArray = [];
-
-          let resDateWorkout = resDates.date;
-          let formatWorkout = new Date(resDateWorkout);
-          let workoutDay = formatWorkout.getDate(resDateWorkout) + 1;
-        // console.log(workoutDay);
-          workoutDates.push(workoutDay);
-       // console.log(workoutDates);
-          //console.log(resDates.UserId);
-          let userNum = resDates.UserId;
-         //console.log(userNum);
-          userArray.push(userNum);
-          console.log(userArray);
-       // console.log(resDates.date);
-        var mostRecent = Math.max(...workoutDates);
-        workoutArray.push(mostRecent);
-        console.log(workoutArray);
-       // if (mostRecent > 9) {
-        // console.log(mostRecent);
-          let mostRecentDate = "2019-03-" + mostRecent;
-          console.log(resDates.date);
-
-          if (mostRecentDate === resDates.date) {
-            console.log(mostRecentDate);
-            console.log(resDates.name);
-            $(".dateDiv").append(mostRecentDate);
-            $(".name").append(resDates.name);
+            let resDateWorkout = results[i].date;
+            let formatWorkout = new Date(resDateWorkout);
+            let workoutDay = formatWorkout.getDate(resDateWorkout) + 1;
+            workoutDates.push(workoutDay);
           }
-
-        for (let i = resDates.exercises.length - 1; i < resDates.exercises.length; i++) {
-        //  console.log(resDates.exercises[i]);
-         // console.log(resDates.date)
-          console.log(resDates.exercises[i].specific_exercise);
-          $(".center-div").append(`
-          <p>${resDates.exercises[i].specific_exercise}</p>
-          `)
-          console.log(resDates.exercises[i].sets);
-          sets = resDates.exercises[i].sets;
-          sets.forEach(set => {
-            $(".center-div").append(`
-            <p>${set.reps} reps x ${set.weight} lbs
-            `)
-          })
- 
         };
-    
-     // };
-      };
-    }); 
+        let mostRecent = Math.max(...workoutDates);
+        let mostRecentDate = "2019-03-" + mostRecent;
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].UserId === activeUser) {
+            if (results[i].date === mostRecentDate) {
 
+              $(".dateDiv").append(mostRecentDate);
+              $(".name").append(results[i].name);
+              let exercises = results[i].exercises;
+              exercises.forEach(exercise => {
+                $(".center-div").append(`
+                <p>${exercise.muscle_group}</p>
+                `)
+              });
+              $(".center-div").append(`
+              <p>${results[i].exercises[0].specific_exercise}</p>
+              `)
+              let sets = results[i].exercises[0].sets
+              sets.forEach(set => {
+                $(".center-div").append(`
+                <p>${set.reps} reps x ${set.weight} lbs</p>
+                `)
+              });
+            };
+          };
+        }
       });
     });
   }
   printWorkout();
 });
-  
